3972dcd95c4ebc56ce5f4d6d0c79a0d735fb41e6
Kayla <kayla_latorre@dlsu.edu.ph>
2021-08-14 23:05:37 +0800

    added activity_landing, activity_login, activity_signup, activity_profile

---
+++ app/src/main/java/com/mobdeve/s15/group14/fudecide/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s15.group14.fudecide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
