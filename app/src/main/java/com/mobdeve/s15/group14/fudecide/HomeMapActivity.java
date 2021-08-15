1bae472509885b85346626e3693b33572d2eca93
Kayla <kayla_latorre@dlsu.edu.ph>
2021-08-15 18:05:57 +0800

    added activity_home_main, activity_home_map; edited profile

--- app/src/main/java/com/mobdeve/s15/group14/fudecide/HomeMapActivity.java
+++ app/src/main/java/com/mobdeve/s15/group14/fudecide/HomeMapActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomeListviewActivity extends AppCompatActivity {
+public class HomeMapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_listview);
+        setContentView(R.layout.activity_home_map);
     }
 }
