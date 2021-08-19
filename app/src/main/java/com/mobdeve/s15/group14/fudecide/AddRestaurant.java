9d19099d21ac36a22a22730d94ab1860aeac862d
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2021-08-19 13:51:59 +0800

    Created dummy AddRestaurant function

---
+++ app/src/main/java/com/mobdeve/s15/group14/fudecide/AddRestaurant.java
@@ -0,0 +1,19 @@
+package com.mobdeve.s15.group14.fudecide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+    Sample function to avoid manually inputting restaurants in the db
+    TASKS:
+        [ ] Add restaurant details
+        [ ] Add multiple menu items
+ */
+public class AddRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+    }
+}
