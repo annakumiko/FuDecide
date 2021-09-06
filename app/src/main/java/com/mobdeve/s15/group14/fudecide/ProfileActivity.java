57eb53e0f752ac8299a25d3c308a876f4d283eac
josh <joshua_fabian@dlsu.edu.ph>
2021-09-06 17:36:48 +0800

    Updated register and view profile

--- app/src/main/java/com/mobdeve/s15/group14/fudecide/ProfileActivity.java
+++ app/src/main/java/com/mobdeve/s15/group14/fudecide/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.mobdeve.s15.group14.fudecide;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +25,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,11 +39,26 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private String userID;
 
+    private int googleSignIn;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    LoginActivity la = new LoginActivity();
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore fs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        fs = FirebaseFirestore.getInstance();
+
+        googleSignIn = la.getGoogleSignIn();
+        Toast.makeText(ProfileActivity.this, "googleSignIn = " + googleSignIn, Toast.LENGTH_LONG).show();
 
         home = (ImageView) findViewById(R.id.btn_home);
         home.setOnClickListener(this);
@@ -42,27 +69,36 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         user = FirebaseAuth.getInstance().getCurrentUser();
         dbRef = FirebaseDatabase.getInstance().getReference("Users");
 
-        userID = user.getUid();
-
         final TextView userNameTextView = (TextView) findViewById(R.id.tv_user);
+        final TextView bioTextView = (TextView) findViewById(R.id.tv_bio);
 
-        dbRef.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                UserModel userProfile = snapshot.getValue(UserModel.class);
 
-                if(userProfile != null){
-                    String name = userProfile.name;
-
-                    userNameTextView.setText("Hello, " + name);
+        // OnCreate if Firebase Auth is used
+        if (googleSignIn == 0){
+            userID = mAuth.getCurrentUser().getUid();
+            DocumentReference documentReference = fs.collection("users").document(userID);
+            documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                    userNameTextView.setText("Hello, " + documentSnapshot.getString("name"));
+                    bioTextView.setText(documentSnapshot.getString("bio"));
                 }
-            }
+            });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(ProfileActivity.this, "Error getting user information.", Toast.LENGTH_LONG).show();
+        // OnCreate if Google Sign In is used
+        } else if (googleSignIn == 1){
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
+            // Set tv_user to name
+            if (acct != null) {
+                String name = acct.getGivenName();
+                userNameTextView.setText("Hello, " + name);
             }
-        });
+        }
     }
 
     @Override
@@ -73,9 +109,30 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.btn_logout:
                 Toast.makeText(ProfileActivity.this, "Signing out", Toast.LENGTH_LONG).show();
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(this, LandingActivity.class));
-                break;
+                signOut();
+        }
+    }
+
+    // signOut function
+    private void signOut() {
+        //Intent intent = new Intent(ProfileActivity.this, LandingActivity.class);
+
+        // signOut for Firebase
+        if (googleSignIn == 0){
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(ProfileActivity.this, LandingActivity.class));
+            finish();
+
+        // signOut for Google
+        } else if (googleSignIn == 1){
+            mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    startActivity(new Intent(ProfileActivity.this, LandingActivity.class));
+                    finish();
+                }
+            });
         }
     }
 }
